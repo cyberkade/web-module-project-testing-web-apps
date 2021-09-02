ea45f09f1d8fff7c94a32b8e6c618441cf31f320
Kade Griffith <kadeg95@gmail.com>
2021-09-02 10:12:08 -0600

    brainstormed and wrote out a few tests. trying to find out how to select the submit input

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import {render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-
 import ContactForm from './ContactForm';
 
 test('renders without errors', ()=>{
-    
+    render(<ContactForm />) 
 });
 
 test('renders the contact form header', ()=> {
-    
+    render(<ContactForm />) 
+    const header = screen.getByText(/contact form/i)
+    expect(header).toBeInTheDocument();
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+    render(<ContactForm />)
+    const firstName = screen.getByPlaceholderText('Edd')
+    userEvent.type(firstName, 'Kade')
+    expect(screen.getByText('Error: firstName must have at least 5 characters.'))
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
-    
+    render(<ContactForm />)
+    const submitBtn = screen.get
+    expect(screen.getByText(/Error: firstName must have at least 5 characters./i))
+    expect(screen.getByText(/Error: lastName is a required field./i))
+    expect(screen.getByText(/Error: email must be a valid email address./i))
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
+    render(<ContactForm />)
+    const firstName = screen.getByPlaceholderText('Edd')
+    userEvent.type(firstName, 'Kade')
+    const lastName = screen.getByPlaceholderText('Burke')
+    userEvent.type(lastName, 'Griffith')
+
     
 });
 
